cf10e9a2a60a7948ac7020636a66849bd26fdc60
Kristian <mkristian@live.ru>
2020-02-01 22:55:54 +0400

    Updates

---
+++ precache-manifest.1bcc823a19fb340c84a97c333484e195.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9665e6f1b6797e47b171f404bfdcd9",
+    "url": "/swbase/index.html"
+  },
+  {
+    "revision": "bc4cb328c3890b689b57",
+    "url": "/swbase/static/css/main.d603eda6.chunk.css"
+  },
+  {
+    "revision": "477f5329154e847e3af9",
+    "url": "/swbase/static/js/2.ee675cf0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/swbase/static/js/2.ee675cf0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc4cb328c3890b689b57",
+    "url": "/swbase/static/js/main.e2198f37.chunk.js"
+  },
+  {
+    "revision": "be1596c299ca1d3ed2dc",
+    "url": "/swbase/static/js/runtime-main.a838d02d.js"
+  },
+  {
+    "revision": "b673826f92a14633e2b00c5b89b1c94b",
+    "url": "/swbase/static/media/bg.b673826f.png"
+  },
+  {
+    "revision": "7eb176e23eb9f63a2868bac75d87b277",
+    "url": "/swbase/static/media/starwars-glyphicons-webfont.7eb176e2.ttf"
+  }
+]);
